0f9f1a3fe65564b568ae2502f71ceaf75d7c455f
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-02-14 11:36:31 -0600

    in progress

---
+++ src/main/java/Student.java
@@ -0,0 +1,35 @@
+package main.java;
+
+import java.util.ArrayList;
+
+
+public class Student {
+
+    private String name;
+    private long id;
+    private ArrayList<Integer> grades;
+
+    // returns the student's id
+    public long getId() {
+        return this.id;
+    }
+
+    // returns the student's name
+    public String getName() {
+        return this.name;
+    }
+
+    // adds the given grade to the grades property
+    public void addGrade(int grade) {
+        this.grades.add(grade);
+    }
+
+    // returns the average of the students grades
+    public double getGradeAverage() {
+        double sum = 0;
+        for (Integer grade : grades) {
+            sum += grade;
+        }
+        return sum / grades.size();
+    }
+}
